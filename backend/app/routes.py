35bf5901c7d21ac9031190818e285054567ad087
jonl-greer <jlgreer@uchicago.edu>
2026-04-02 17:27:02 -0500

    Update routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -8,8 +8,8 @@ router = APIRouter()
 @router.get("/demo")
 
 def run_demo():
-  election = gpd.read_file("/data/pa_vest_20.zip")
-  precints = gpd.read_file("/data/pa_vtd_2020_bound.zip")
+  election = gpd.read_file("zip:///data/pa_vest_20.zip")
+  precincts = gpd.read_file("zip:///data/pa_vtd_2020_bound.zip")
   result = validate_data(precints,elections)
   return result.to_json()
 
