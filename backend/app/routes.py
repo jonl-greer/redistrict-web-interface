3c7aa6fb32df5d0e851c60e30d8128b3c23883e9
jonl-greer <jlgreer@uchicago.edu>
2026-04-02 14:20:03 -0500

    Update routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,1 +1,15 @@
+from fastapi import APIRouter
+import geopandas as gpd
+import pandas as pd
+from .validation import validate_data
+
+router = APIRouter()
+
+@router.get("\demo")
+
+def run_demo():
+  gdf1 = gdp.read_file("backend/data/pa_vest_20.zip")
+  gdf2 = gdp.read_file("back/data/pa_vtd_2020_bound.zip")
+  gdf1 = validate_data(gdf,df)
+  return gdf.to_json()
 
