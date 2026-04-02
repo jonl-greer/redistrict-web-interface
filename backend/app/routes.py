8fdca08d5a638be0f219d065d330dbcbc61dbb50
jonl-greer <jlgreer@uchicago.edu>
2026-04-02 14:27:40 -0500

    Update routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -8,8 +8,8 @@ router = APIRouter()
 @router.get("\demo")
 
 def run_demo():
-  gdf1 = gdp.read_file("backend/data/pa_vest_20.zip")
-  gdf2 = gdp.read_file("back/data/pa_vtd_2020_bound.zip")
-  gdf1 = validate_data(gdf,df)
-  return gdf.to_json()
+  election = gdp.read_file("backend/data/pa_vest_20.zip")
+  precints = gdp.read_file("back/data/pa_vtd_2020_bound.zip")
+  result = validate_data(precints,elections)
+  return result.to_json()
 
