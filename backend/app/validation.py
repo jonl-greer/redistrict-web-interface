b7dd60c3bbb5684e2da65c7349865a16b71d1978
jonl-greer <jlgreer@uchicago.edu>
2026-04-02 14:31:25 -0500

    Update validation.py

--- backend/app/validation.py
+++ backend/app/validation.py
@@ -1,1 +1,23 @@
+from rapidfuzz import process
 
+def validate_data(precints,election):
+  precinct_names = precincts.iloc[:,0].astype(str).tolist()
+  election_names = election.iloc[:,0].astype(str).tolist()
+
+  statuses = []
+  matches = []
+
+  for name in precint_names:
+    match,score,_= process.extractOne(name, election_names)
+    matches.append(math)
+
+    if score < 70:
+      statuses.append("bad")
+    elif score < 90:
+      statuses.append("warning")
+    else:
+      statuses.append("good")
+
+  precints["matched_name"] = matches
+  precints["status"] = statuses 
+  return precincts
