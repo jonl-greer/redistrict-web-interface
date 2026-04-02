d8b8751d9b1048636f004037760967d40f5ee84d
jonl-greer <jlgreer@uchicago.edu>
2026-04-02 18:43:28 -0500

    Update routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -9,8 +9,8 @@ DATA_DIR = Path(__file__).parent.parent / "data"
 @router.get("/demo")
 
 def run_demo():
-  election = gpd.read_file("pa_vest_20.zip")
-  precincts = gpd.read_file("pa_vtd_2020_bound.zip")
+  election = gpd.read_file(DATA_DIR / "pa_vest_20.zip")
+  precincts = gpd.read_file(DATA_DIR / "pa_vtd_2020_bound.zip")
   result = validate_data(precincts,election)
   return result.to_json()
 
